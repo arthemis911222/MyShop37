d6bdefb03eb000dd0d7dd8f907185d6f358e329c
wujiajie <527028589@qq.com>
2017-01-04 21:08:27 +0800

    产品管理、分类管理、订单管理、用户管理、管理员管理页面设计和控制器框架

---
+++ MyShopDesign/MyShopDesign/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShopDesign.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            return RedirectToAction("index");
+        }
+
+        public ActionResult ResetPassword(int Id)
+        {
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ MyShopDesign/MyShopDesign/Areas/Admin/Controllers/WorkersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShopDesign.Areas.Admin.Controllers
+{
+    public class WorkersController : Controller
+    {
+        // GET: Admin/Workers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult AddSave()
+        {
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return View();
+        }
+
+        public ActionResult EditSave()
+        {
+            return RedirectToAction("index");
+        }
+
+    }
+}
